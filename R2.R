e585c501cfc176832d89de9a5999944ebb3954ce
ekatnic <ethkatnic@gmail.com>
2019-07-03 20:05:33 -0700

    work on R2

--- R2.R
+++ R2.R
@@ -1,99 +1,130 @@
 # A. Conditional Statements and Loops – Using R
-a <- c(1:8)
-b <- c("meep", "foo", "meep", "mod", "foo", "mod", "meep", "foo")
-d <- cbind(a, b)
-for(i in 1:nrow(d)) {
-  if(d[i,2] == "meep"){
-    print("oops")
-  } else if (d[i,2] == "foo") {
-    print("ooh")
+sum5 <- function(num){
+  if(num < 0){
+    print("error")
   } else {
-    print("yay")
+    sum = 0
+    while(num > 0){
+      sum = sum + num
+      num = num - 1
+    }
+    print(sum)
   }
 }
 
-# B. Data Management – Using R
-Manager <- function(n){
-  
+# TODO B.Data Management – Using R
+library(readxl)
+library(dplyr)
+setwd("~/Desktop/work_prep")
+data <- read_excel("data/cars.xlsm")
+
+outlier_detect <- function(column_name){
+  column_data <- data[[column_name]]
+  data_iqr <- IQR(column_data)
+  q1 <- quantile(column_data, .25)
+  q2 <- quantile(column_data, .75)
+  top_cutoff <- q2 + data_iqr*1.5
+  bottom_cutoff <- q1 - data_iqr*1.5
+  outliers <- column_data[(column_data > top_cutoff | column_data < bottom_cutoff)]
+  print(paste("Outliers:"))
+  print(outliers)
+  return(outliers)
 }
 
+replace_outliers <- function(column_name){
+  outliers <- outlier_detect(column_name)
+  outlier_frame <- data[data[[column_name]] %in% outliers,]
+  View(outlier_frame)
+  if(TRUE){
+    data[data[[column_name]] %in% outliers, column_name] <- mean(data[[column_name]])
+  }
+  return(data)
+}
+
+data <- replace_outliers("V10")
+
 # C. User Defined Functions – Using R
-#    4)
-x <- c(1, 3, 5, 7, 10)
-y <- c(1, 5, 10, 12, 14)
-setdiff(x, y)
-# One could alternatively loop through x, checking if val is not contained in y
-x[!is.element(x,y)]
+#   4)
+math_func <- function(vector){
+  return(vector[1] + vector[2] * vector[3] - vector[4] / vector[5])
+}
+math_func(c(1,2,3,4,5))
 
-#    5)
+#   5)
+#   GIVEN DATA DIFFERS IN NUMBER OF ROWS, ADDED NA TO END OF AGE COLUMN
+sampledata <- data.frame(Marks = c(80,75,85,65,55,NA,NA,89,NA), 
+                         age =   c(21,23,27,25,26,26,23,NA, NA) )
 #     a)
-data_try <- data.frame(salary = c(20000,30000,50000,40000,60000,NA,-
-                                       4000,25000),
-                          age = c(21,23,27,25,26,26,23,NA) )
-
 impute_data <- function(column){
-  replace(column, which(column < 0 | is.na(column)), median(column, na.rm=TRUE))
+  replace(column, which(is.na(column)), mean(column, na.rm=TRUE))
 }
 
 #     b)
-data_try <- apply(data_try, 2, impute_data)
+sampledata <- apply(sampledata, 2, impute_data)
 
+#
 # D. Descriptive Statistics
- # 6)
-  # a)
+#   6)
+#     a)
 library(MASS)
 library(dplyr)
-data <- c(42.52, 43.83, 37.14, 43.22, 40.30, 34.11, 42.88, 45.37, 43.33, 43.96,
-          40.94, 37.20, 40.70, 42.83, 48.36, 38.74, 42.25, 41.53, 43.96, 50.99,
-          43.65, 48.21, 38.22, 43.20, 31.90, 45.63, 41.55, 49.03, 39.69, 42.02)
+data <- c(23.20, 24.44, 25.29, 26.27, 28.11, 29.32, 30.36, 35.14, 36.19, 36.25,
+          40.29, 42.27, 43.11, 44.20, 46.44, 46.36, 46.14, 48.19, 48.12, 48.26, 
+          48.25, 49.20, 49.32, 49.36, 50.26, 53.28, 53.19, 70.14, 71.22, 85.10)
 data_range <- range(data) 
-breaks <- seq(floor(data_range[1]), ceiling(data_range[2]), by=1) 
+breaks <- seq(floor(data_range[1]), ceiling(data_range[2]), by=3) 
 interval <- cut(data, breaks, right=FALSE)
 data_table <- table(interval)
 data_table <- data_table %>% transform(Rel_Freq = prop.table(Freq), Per_Freq = prop.table(Freq) * 100, Cum_Freq = cumsum(Freq))
 write.csv(data_table, file = "~/Desktop/data2.csv")
-  # b)
-  #TODO: import photo
-  # c)
-  #    The histogram to indicates an approximately symmetric, bell-shaped distribution, as the majority
-  #    of values fall between the 40-45 range, although there is a slightly longer left tail, indicating
-  #    a very minor left skew.
-  # d)
+#     b)
+#TODO: import photo
+#     c)
+#       The histogram appears to be moderately skewed right due to some extreme values on the upper end.
+#     d)
 data_sd <- sd(data)
-data_iqr <- IQR(data)
-  # e)
+data_iqr <- IQR(data) 
+#     e)
 q1 <- quantile(data, .25)
 q2 <- quantile(data, .75)
 top_cutoff <- q2 + data_iqr*1.5
 bottom_cutoff <- q1 - data_iqr*1.5
 outliers <- data[(data > top_cutoff | data < bottom_cutoff)]
-  # f)
-  #    While the data is approximately symmetric distribution and calculating mean would be a reasonable choice,
-  #    the median is likely a bit more accuarte, due to the presence of more left outliers than right.
-#   g)
-  #    One could use a square/cube transformation or add some constant and use a logarithmic transformation.
+#     f)
+#       The most appropriate central tendency measure for this data would be median, as it is skewed
+#       and using mean would be influenced by these outliers.
+#     g)
+#       A logarithmic transformation would likely be most appropriate, as there are not many zeroes.
+#       Using log(x+1) would be appropriate if any zeroes are in the dataset. 
 # 7)
-values <- c(42.52,40.94,NA,39.69,43.83,37.2,NA,42.02,37.14,40.7,43.65,43.22,42.83,48.21,40.3,48.36,38.22,34.11,
-            38.74,43.2,42.88,42.25,31.9,45.37,41.53,45.63,43.33,43.96,41.55,43.96,50.99,49.0)
+values <- data.frame(data = c(23.2,24.4,25.2,26.2,28.1,29.3,30.3,35.1,36.1, 36.2, 
+                                40.2, 42.2, 43.1, 44.2, 46.4, 46.3, 46.1, 48.1, 48.1,
+                                48.2, NA, NA, 48.2, 49.2, 49.3, 49.3, 50.2, 53.2, 53.1,
+                                70.1, 71.2, 85.1), 
+                         Freq =   c(0, 4, 9, 7, 1, 2, 6, 4, 9, 5, 9, 8, 1, 0, 4, 6, 4,
+                                    9, 2, 6, NA, NA, 5, 0, 2, 6, 6, 8, 9, 4, 2, 0) )
 values <- na.omit(values)
-values_range <- range(values) 
-value_breaks <- seq(floor(values_range[1]), ceiling(values_range[2]), by=1) 
-value_intervals <- cut(values, value_breaks, right=FALSE)
-value_table <- table(value_intervals)
-value_table <- value_table %>% transform(Rel_Freq = prop.table(Freq), Cum_Freq = cumsum(Freq))
-value_table$Percent_Frequency <- value_table$Rel_Freq * 100
+values_range <- range(values$data) 
+by <- 4
+value_breaks <- seq(floor(values_range[1]), ceiling(values_range[2]) + by - ceiling(values_range[2]) %% by,
+                    by=by) 
+value_intervals <- cut(values$data, value_breaks, right=FALSE)
+values$interval <- value_intervals
+values <- values %>% group_by(interval) %>% summarise(total_freq = sum(Freq))
+values <- values %>% transform(Rel_Freq = prop.table(total_freq), Cum_Freq = cumsum(total_freq))
+values$Percent_Frequency <- values$Rel_Freq * 100
 # MIDPOINTS FUNCTION CODE FROM https://www.r-bloggers.com/finding-the-midpoint-when-creating-intervals/
 midpoints <- function(x, dp=2){
   lower <- as.numeric(gsub(",.*","",gsub("\\(|\\[|\\)|\\]","", x)))
   upper <- as.numeric(gsub(".*,","",gsub("\\(|\\[|\\)|\\]","", x)))
   return(round(lower+(upper-lower)/2, dp))
 }
-value_table$Midpoint <- sort(midpoints(value_table$value_intervals))
-value_table <- value_table[,c(1, 6, 2, 3, 5, 4)]
-colnames(value_table) <- c("Interval", "Midpoint", "Absolute Frequency", "Relative Frequency",
+values$Midpoint <- sort(midpoints(values$interval))
+values <- values[,c(1, 6, 2, 3, 5, 4)]
+colnames(values) <- c("Interval", "Midpoint", "Absolute Frequency", "Relative Frequency",
                            "Percent Frequency", "Cumulative Frequency")
 
-#    b)
+# TODO b)
 mean <- mean(values)
 median <- median(values)
 quartile1 <- quantile(values, 0.25)
@@ -102,7 +133,7 @@ stand_dev <- sd(values)
 
 #    c)
 boxplot <- boxplot(values, ylab = "value",
-        main = "Value Boxplot")
+                   main = "Value Boxplot")
 #    d)
 simple_table <- as.data.frame(values)
 library(ggplot2)
@@ -110,30 +141,16 @@ histo <- ggplot(simple_table, aes(x=values)) +
   geom_histogram(color="black", fill="white", binwidth = 1) + ggtitle("Value Histogram") +
   theme(plot.title = element_text(hjust = 0.5))
 
-# E. Probability Theory
-# Unsure how to run completely in R, struggling to find information online
-# Total umber of outcomes of sample space: (10 choose 3) 
-possible_outcomes <- choose(10, 3)
-# Each outcome in sample space holds an equally likely probability of being filled
-# 8 outcomes where there are 3 consecutive empty spots
-8 / possible_outcomes
+# E.
 
+# F. 
 
+# G. Statistical Distributions – I and II
+#   This problem follows a binomial distribution where n = 10, p = .1
+#   P(X > 3) = 1 - (P(X = 0) + P(X = 1) + P(X = 2) + P(X = 3))
+1 - (dbinom(0, 10, .1) + dbinom(1, 10, .1) + dbinom(2, 10, .1) + dbinom(3, 10, .1))
 
-# F. Sampling Theory
-library("PracTools")
-output <- strAlloc(n.tot = 36, Nh = c(10000,10000), Sh = c(10.27, 6.67), alloc = "neyman")
-num_boys <- output$nh[1]
-
-# G. Statistical Distributions
-#   a)
-#     i)
-pnorm(25, mean = 25.8, sd = .5, lower.tail = TRUE)
-#     ii)
-pnorm(26.5, mean = 25.8, sd = .5, lower.tail=TRUE) - 
-  pnorm(25.5, mean = 25.8, sd = .5, lower.tail=TRUE)
-
-# H. Plots/Charts – Using R
+# H. Plots/Charts – Using R  **SAME AS IN R1.R FILE, COPY AND PASTED**
 options(scipen=10000)
 #   a) I was unsure of how to distinguish and label cities due to the high volume of cities, so I chose to remove the legend
 #      entirely. Labeling each involved excessive overlap and showing a color legend took up extreme space. 
@@ -142,7 +159,7 @@ ggplot(data=grouped_data, aes(x=year, y=sum, group=city, color = city)) +
   scale_y_continuous(labels = scales::comma) + geom_line() + xlab("Date") +
   ylab("Volume (Dollars)") +ggtitle("Texas Annual Volume by City") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) 
-  
+
 
 #   b)
 ggplot(data = grouped_data, aes(x = year, y = sum,fill=city)) +
@@ -156,7 +173,7 @@ ggplot(data = grouped_data, aes(x = year, y = sum,fill=city)) +
 #     i)
 library(zoo)
 top_cities <- txhousing %>% group_by(city) %>% summarise(sum = sum(volume, na.rm = TRUE)) %>% 
-                                                           top_n(5, sum)
+  top_n(5, sum)
 top_cities_monthly <- txhousing %>% group_by(city) %>% filter(city %in% top_cities$city)
 top_cities_monthly$chrono_month <- as.yearmon(with(top_cities_monthly, paste(year, month, sep="-")), "%Y-%m")
 
@@ -166,7 +183,7 @@ ggplot(data=top_cities_monthly, aes(x=chrono_month, y=volume, group=city, color 
   xlab("Date") + ylab("Volume (dollars)") + 
   ggtitle("Top 5 Texas City Monthly Volume") + 
   theme(plot.title = element_text(hjust = 0.5))
-  
+
 #     ii)
 ggplot(data=top_cities_monthly, aes(x=chrono_month, y=volume, color = city)) +
   scale_y_continuous(labels = scales::comma) + geom_line() +
@@ -184,7 +201,7 @@ ggplot() + geom_point(data =top_2015, aes(city, total_sales, color = "dark green
   xlab("City") + ylab("Total Sales (dollars)") + 
   ggtitle("Top 5 Texas City 2015 Housing Sales") + 
   theme(plot.title = element_text(hjust = 0.5))
-  
+
 #     iv)
 ggplot(top_cities_monthly, aes(x=city, y=sales)) + 
   xlab("City") + ylab("Number of Sales") + 
@@ -203,16 +220,5 @@ ggplot() + geom_point(data= top_cities_monthly, aes(sales, volume, color = city)
   ggtitle("Top 5 Texas City Monthly Housing Sales vs. Volume") + 
   theme(plot.title = element_text(hjust = 0.5))
 
-#  I. Testing of Hypopthesis
-#     a. null, Ho: There is no significant difference between the mean Verbal SAT scores of first year
-#              psychology majors compared to the population
-#        alternative, Ha: There is a signifcant difference between the mean Verbal SAT scores of first year
-#              psychology majors compared to the population
-#     b. Reject null if p-value < .05
-#     c. μ = 520, σ^2 = σ * σ = 95*95 = 9025, M = 548, N = 36
-#        Z = (M - μ) / √(σ^2 / N)
-#        Z = (548 - 520) / √(9025 / 36)
-#        Z = 28 / 15.83333
-#        Z = 1.76842  => α(.05) =>  p = .07672
-#     d. p = .07672 > .05, fail to reject Ho
-#     e. There is no significant difference between the mean score and the scores of first year psych majors
+# I. Testing Of Hypothesis
+
